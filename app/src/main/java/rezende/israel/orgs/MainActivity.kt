1cc22cfdb6bae82f91c3f3f36550fbc16ff5c415
israel <israelrezendeluiz2903@gmail.com>
2022-09-20 08:42:47 -0300

    Realizado configurações iniciais de layout e ById das views.

--- app/src/main/java/rezende/israel/orgs/MainActivity.kt
+++ app/src/main/java/rezende/israel/orgs/MainActivity.kt
@@ -2,13 +2,22 @@ package rezende.israel.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
-
+    
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "App em construção... "+ ("\u26A0"), Toast.LENGTH_LONG).show()
-    }
+        Toast.makeText(this, "App em construção... " + ("\u26A0"), Toast.LENGTH_LONG).show()
+        setContentView(R.layout.activity_main)
 
+        val nome = findViewById<TextView>(R.id.nome)
+        nome.text = "Cesta de Frutas"
+        val descricao = findViewById<TextView>(R.id.descricao)
+        descricao.text = "Laranja, morango e uva"
+        val valor = findViewById<TextView>(R.id.valor)
+        valor.text = "R$ 19,90"
+
+    }
 }
